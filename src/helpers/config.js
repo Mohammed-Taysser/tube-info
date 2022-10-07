2a1c6bf61ed324acf4de3561d12d8dc30ef2b860
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-10-07 17:10:11 +0200

    create `config` file

---
+++ src/helpers/config.js
@@ -0,0 +1,133 @@
+import Conf from 'conf';
+import path from 'path';
+import os from 'os';
+
+const schema = {
+	version: {
+		type: 'string',
+		default: '1.0.0',
+	},
+	apiKey: {
+		type: 'string',
+		default: '',
+	},
+	apiBaseUrl: {
+		type: 'string',
+		default: 'https://www.googleapis.com/youtube/v3',
+		format: 'url',
+	},
+	playlistExportItems: {
+		type: 'object',
+		properties: {
+			position: { type: 'boolean' },
+			title: { type: 'boolean' },
+			uploader: { type: 'boolean' },
+			uploaderUrl: { type: 'boolean' },
+			url: { type: 'boolean' },
+			description: { type: 'boolean' },
+			videoPrivacy: { type: 'boolean' },
+			publishTime: { type: 'boolean' },
+			thumbnail: { type: 'boolean' },
+		},
+		default: {
+			position: true,
+			title: true,
+			uploader: true,
+			uploaderUrl: false,
+			url: true,
+			description: false,
+			videoPrivacy: false,
+			publishTime: false,
+			thumbnail: true,
+		},
+	},
+	fileExt: {
+		type: 'string',
+		enum: ['csv', 'json'],
+		default: 'json',
+	},
+	folderPath: {
+		type: 'string',
+		default: path.join(os.homedir(), 'tube/reports/'),
+	},
+	skipPrivateOrDeleted: {
+		type: 'boolean',
+		default: true,
+	},
+};
+
+class Config {
+	constructor() {
+		this.conf = new Conf({ schema });
+	}
+
+	/**
+	 * Throw an error if the config key is not found.
+	 * @param {string} key  Config key
+	 */
+	checkKey(key) {
+		if (!this.conf.has(key)) {
+			throw new Error('Unknown config key');
+		}
+	}
+
+	get(key) {
+		this.checkKey(key);
+		return this.conf.get(key);
+	}
+
+	set(key, value) {
+		this.checkKey(key);
+		this.conf.set(key, value);
+	}
+
+	/**
+	 * Used in the `default` field of `inquirer` prompt.
+	 * @returns {string[]}  Key names in `exportItems` if that key's value is `true`
+	 */
+	getPlaylistExportItemsDefaults() {
+		const exportItems = this.conf.get('playlistExportItems');
+
+		const result = [];
+
+		Object.keys(exportItems).forEach((key) => {
+			if (exportItems[key]) {
+				result.push(key);
+			}
+		});
+
+		return result;
+	}
+
+	/**
+	 * Set which items to export by default.
+	 * @param {string[]} items Key names in `exportItems` to be set to `true`
+	 */
+	setPlaylistExportItemsDefaults(items) {
+		const exportItems = this.conf.get('playlistExportItems');
+
+		Object.keys(exportItems).forEach((item) => {
+			exportItems[item] = items.includes(item);
+		});
+
+		this.conf.set('playlistExportItems', exportItems);
+	}
+
+	/**
+	 * Get the path of the config file.
+	 */
+	get path() {
+		return this.conf.path;
+	}
+
+	/**
+	 * Reset all items to their default value defined in `schema`.
+	 */
+	resetAll() {
+		this.conf.clear();
+	}
+}
+
+const config = new Config();
+
+export { config as default, schema };
