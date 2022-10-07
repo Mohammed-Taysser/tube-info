1ab7092a99b3f3a37e9f955883bc67e5f39d376e
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-10-07 23:29:07 +0200

    create playlist command

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { Command } from 'commander';
 import config from './helpers/config.js';
-import { apiKeyAction, configAction } from './helpers/actions.js';
+import {
+	apiKeyAction,
+	configAction,
+	playlistAction,
+} from './helpers/actions.js';
 
 const app = new Command();
 
@@ -20,5 +24,23 @@ app
 	.option('-p, --path', 'show the path of the config file')
 	.option('-r, --reset', 'reset all configurations to default')
 	.action(configAction);
+
+app
+	.command('playlist')
+	.description('Export playlist metadata its id or url.')
+	.argument(
+		'ID Or URL',
+		`The value of the "list" parameter in the the playlist homepage URL (https://www.youtube.com/playlist?list='playlistId'`
+	)
+	.action(playlistAction)
+	.option('-d, --default', 'Skip all questions and use the default config')
+	.addHelpText(
+		'after',
+		`
+Example:
+ $ tube-info playlist PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ
+ $ tube-info playlist PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ -d
+    `
+	);
 
 app.parse(process.argv);
