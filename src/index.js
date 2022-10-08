d97c26169f9b27e3ae64c23e963431b713f11709
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-10-08 17:33:13 +0200

    add video command

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {
 	apiKeyAction,
 	configAction,
 	playlistAction,
+	videoAction,
 } from './helpers/actions.js';
 
 const app = new Command();
@@ -30,7 +31,7 @@ app
 	.description('Export playlist metadata its id or url.')
 	.argument(
 		'ID Or URL',
-		`The value of the "list" parameter in the the playlist homepage URL (https://www.youtube.com/playlist?list='playlistId'`
+		`The value of the "list" parameter in the the playlist homepage URL https://www.youtube.com/playlist?list='playlistId'`
 	)
 	.action(playlistAction)
 	.option('-d, --default', 'Skip all questions and use the default config')
@@ -38,8 +39,26 @@ app
 		'after',
 		`
 Example:
- $ tube-info playlist PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ
+ $ tube-info playlist https://www.youtube.com/playlist?list=PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ
  $ tube-info playlist PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ -d
+    `
+	);
+
+app
+	.command('video')
+	.description('Export video metadata by id or url.')
+	.argument(
+		'ID Or URL',
+		`The value of the "v" parameter in the the video URL https://www.youtube.com/watch?v='videoId'`
+	)
+	.action(videoAction)
+	.option('-d, --default', 'Skip all questions and use the default config')
+	.addHelpText(
+		'after',
+		`
+Example:
+ $ tube-info video https://www.youtube.com/watch?v=yUndnE-2osg
+ $ tube-info video yUndnE-2osg -d
     `
 	);
 
